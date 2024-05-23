248f0e8cd7a5dcd23fd7676d1280b30d5e2112ea
lipovskoy <anton@volt.se>
2024-05-23 14:30:39 +0200

    added images

---
+++ src/components/Images.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Images = () => {
+    return (
+        <div>
+        <img src="https://avatars.githubusercontent.com/u/1587060?v=4"></img>
+        </div>
+    )
+}
+
+export default Images
